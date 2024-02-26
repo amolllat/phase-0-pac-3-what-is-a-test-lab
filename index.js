8ddacd333289929f18da1da02852671caaf88d31
Amal Mohammed <157481527+amolllat@users.noreply.github.com>
2024-02-26 10:46:33 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 39;
+// function result(height) {
+// if (height > 0 && height > 40) {
+//     return "Height must be greater than 0 and less than 40";
+// }}
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
